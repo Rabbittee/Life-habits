b59d77c767ebb6aba4460544326f74df219080f7
zoeGuava <zoeGuava@gmail.com>
2022-05-12 16:51:58 +0800

    feat: use react-router <Outlet> to set Layout
    - set nested routes
    - set header and footer components at Layout.jsx
    - use index to set default route with the <Home /> page

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import { Login, Home, Diet, RingFit } from 'pages';
+import { Layout } from 'components/layout';
 
 function MainRouter() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="diet" element={<Diet />} />
-        <Route path="ring-fit" element={<RingFit />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="diet" element={<Diet />} />
+          <Route path="ring-fit" element={<RingFit />} />
+        </Route>
       </Routes>
     </>
   );
