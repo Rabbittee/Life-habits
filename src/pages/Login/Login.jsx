9aeb4d9ea79b5f9badf9312b1ae79e3527fafa5e
Pencil <zoeguava@gmail.com>
2022-05-22 16:44:50 +0800

    feat: 🎸 get login form data

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,46 @@
 import { Link } from 'react-router-dom';
 import { checkAccount } from 'components/utils/CheckLogin';
+import { useState } from 'react';
 
 function Login() {
   checkAccount('test data');
 
+  const initialFormData = {
+    email: '',
+    password: '',
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value.trim(),
+    });
+  }
+
+  function handleLogin(event) {
+    event.preventDefault();
+    console.log('formData: ', formData);
+  }
+
   return (
     <section>
       <h1>登入</h1>
-      <form action="submit">
+      <form action="submit" onSubmit={handleLogin}>
         <label>
           <p>信箱</p>
-          <input type="email" />
+          <input type="email" name="email" onChange={handleChange} />
         </label>
 
         <label>
           <p>密碼</p>
-          <input type="password" />
+          <input type="password" name="password" onChange={handleChange} />
         </label>
 
         <label>
-          <input type="button" value="登入" />
+          <input type="button" value="登入" onClick={handleLogin} />
         </label>
       </form>
       <p>還沒有帳號嗎？</p>
