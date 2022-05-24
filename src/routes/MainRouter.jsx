113c7346fcaa95241382df7eaafcc05ad84cfc29
Pencil <zoeGuava@gmail.com>
2022-05-25 00:43:29 +0800

    feat: add conditional redirect in MainRouter

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Login, SignUp, Home, Diet, RingFit } from 'pages';
 import { Layout, AccountLayout } from 'components/layout';
 
 function MainRouter() {
+  const shouldRedirect = true;
+
   return (
     <>
       <Routes>
         {/* Logged in */}
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={shouldRedirect ? <Navigate replace to="/account" /> : <Home />} />
           <Route path="diet" element={<Diet />} />
           <Route path="ring-fit" element={<RingFit />} />
         </Route>
