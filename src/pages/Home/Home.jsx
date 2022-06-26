ba827c4cb98dd890e69d074a57410d830299b411
Pencil <zoeGuava@gmail.com>
2022-06-26 16:42:38 +0800

    feat: add home route

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
-function Home() {
+function Home({ onLogin }) {
   return (
     <section className="px-5">
       <h2 className="text-center text-2xl">Life Habits Home Page</h2>
+
+      <button
+        type="button"
+        onClick={onLogin}
+        className="p-3 bg-gray-700 text-gray-100 hover:bg-gray-200 hover:text-gray-600"
+      >
+        LoginButton
+      </button>
     </section>
   );
 }
